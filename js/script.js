9796f19acb5beb3e650c5dea0726d8c496d881b1
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-28 16:51:47 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Johanna Liu
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-parallelogram').value)
+  const height = parseInt(document.getElementById('height-of-parallelogram').value)
+
+  // process
+  const area = ( base * height )
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area.toFixed(2) + ' cm²'
+}
